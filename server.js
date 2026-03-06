e372cd0026b233ff92f17aa3e671b74865aad5d0
Sidharth Santhosh <zidhuxdofficial@gmail.com>
2026-03-06 16:37:32 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,26 +1,22 @@
-const express = require("express")
-const http = require("http")
-const {Server} = require("socket.io")
+const express=require("express")
+const http=require("http")
+const {Server}=require("socket.io")
 
-const app = express()
-const server = http.createServer(app)
-const io = new Server(server)
+const app=express()
+const server=http.createServer(app)
+const io=new Server(server)
 
 app.use(express.static("public"))
 
-const rooms = {}
+const rooms={}
 
-io.on("connection", socket=>{
+io.on("connection",socket=>{
 
-socket.on("join", room=>{
+socket.on("join",room=>{
 
 socket.join(room)
 
-if(!rooms[room]){
-rooms[room]=1
-}else{
-rooms[room]++
-}
+rooms[room]=(rooms[room]||0)+1
 
 if(rooms[room]==2){
 io.to(room).emit("ready")
@@ -28,20 +24,18 @@ io.to(room).emit("ready")
 
 })
 
-socket.on("offer", data=>{
-socket.broadcast.emit("offer", data)
+socket.on("offer",data=>{
+socket.to(data.room).emit("offer",data.offer)
 })
 
-socket.on("answer", data=>{
-socket.broadcast.emit("answer", data)
+socket.on("answer",data=>{
+socket.to(data.room).emit("answer",data.answer)
 })
 
-socket.on("ice", data=>{
-socket.broadcast.emit("ice", data)
+socket.on("ice",data=>{
+socket.to(data.room).emit("ice",data.candidate)
 })
 
 })
 
-server.listen(3000, ()=>{
-console.log("server running")
-})
+server.listen(process.env.PORT||3000)
